dcfd89c11c42d36b6e1880ee4787dde79c041fb0
Suha Monir <nahmonir@gmail.com>
2023-07-31 11:47:40 -0400

    Initial Commit

---
+++ Grantlykwk/GrantlykwkApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrantlykwkApp.swift
+//  Grantlykwk
+//
+//  Created by Scholar on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct GrantlykwkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
