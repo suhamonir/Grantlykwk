f8f1e8144505e7d53ae0c0a0aa472bec0c11b5eb
Suha Monir <nahmonir@gmail.com>
2023-08-01 12:08:55 -0400

    added new views + colors

--- Grantlykwk/ContentView.swift
+++ Grantlykwk/ContentView.swift
@@ -7,15 +7,53 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        
+        let darkBlue = UIColor(red: 0.00784313725490196, green: 0.18823529411764706, blue: 0.2784313725490196, alpha: 1)
+        
+        let lightBlue = UIColor(red: 0.5568627450980392, green: 0.792156862745098, blue: 0.9019607843137255, alpha: 1)
+ 
+    
+        
+        
+        
+        
+        
+        NavigationStack{
+            
+            
+            
+              VStack {
+                Text("Grantly")
+                  .font(.largeTitle)
+                  .fontWeight(.heavy)
+                NavigationLink(destination: SignUpView()) {
+                  Text("Sign Up")
+                  }
+                NavigationLink(destination: LogInView()) {
+                  Text("Log In")
+                }
+              }
+            
+            
+            
+            
+            
+            }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 }
 
---
+++ Grantlykwk/LogInView.swift
@@ -0,0 +1,20 @@
+//
+//  LogInView.swift
+//  Grantlykwk
+//
+//  Created by Scholar on 8/1/23.
+//
+
+import SwiftUI
+
+struct LogInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LogInView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogInView()
+    }
+}
